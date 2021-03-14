b416a4cb48392ba7a4da699e0917e76d31560fa7
natanael.leite <natansl@gmail.com>
2021-03-14 12:55:12 -0300

    fix: Corrige erro no processo de remover task

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -17,22 +17,25 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return;
+
+    let objTasks = [
+      ...tasks, 
+      { id: (randomId + 1) , title: newTaskTitle, isComplete: false }
+    ];
+
     setRandomId(randomId + 1);
-
-    if(newTaskTitle.length){
-      let objTasks = [...tasks, { id: randomId, title: newTaskTitle, isComplete: false }];
-      setTasks(objTasks);
-    }else{
-      console.log(`O nome da tarefa é obrigatorio!`);
-    }
-
+    setTasks(objTasks);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
-    let task = tasks.filter((data) => data.id === id);
-    let newTask = [...tasks]; // faz uma copia do array anterior
-    newTask[task[0].id] = { id: task[0].id, title: task[0].title, isComplete: !task[0].isComplete }
+    let newTask = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task);
+
     setTasks(newTask);
   }
 
